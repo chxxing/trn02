28a9b2bb524e0991c23d7781a7d251cb17d065f7
chxxing <choing26@naver.com>
2022-06-23 14:54:04 +0900

    220623

--- js/main.js
+++ js/main.js
@@ -45,4 +45,56 @@ $(function () {
         e.preventDefault();
         $('#utb').YTPFullscreen();
     });
-})
+
+    $('.productSlider').slick({
+        arrows: false,
+        dots: true,
+        slidesToShow: 5,
+        centerMode: true,
+    });
+
+    $('.product .productArrows i:nth-child(1)').on('click', function () {
+        $('.productSlider').slick('slickPrev')
+    });
+
+    $('.product .productArrows i:nth-child(2)').on('click', function () {
+        $('.productSlider').slick('slickNext')
+    });
+
+    $('.tab_menu li').on('click', function (event) {
+        event.preventDefault();
+        // console.log($(this), $(this).index(), event.target, event.currentTarget);
+        $('.tab_menu li').removeClass('on');
+        $(this).addClass('on');
+        var idx = $(this).index(); //0, 1, 2
+        $('.tab_content>div').removeClass('on')
+        $('.tab_content>div').eq(idx).addClass('on');
+
+    });
+
+    $('.tab_content ul li').on('click', function (e) {
+        e.preventDefault();
+        var idx = $(this).index();
+        $('.right_content>li').removeClass('on')
+        $('.right_content>li').eq(idx).addClass('on');
+    });
+
+
+    $('.toTop').on('click', function () {
+        $('html,body').animate({ scrollTop: 0 }, 200)
+    });
+
+    $(window).on('scroll', function () {
+        var sct = $(window).scrollTop();
+        if (sct > 500) {
+            $('.toTop').fadeIn(300)
+        } else {
+            $('.toTop').fadeOut(1000)
+        }
+    });
+
+});
+
+
+
+
